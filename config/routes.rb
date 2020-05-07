0e55520b77f52cc96d06ddcc4399b605d91502b8
Kate Howard <k.l.howard.1992@gmail.com>
2020-05-07 16:26:25 +0100

    Add menus and options

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :organisations
+  resources :menus
+  resources :options
   root 'home#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_06_223900) do
+ActiveRecord::Schema.define(version: 2020_05_07_150802) do
+
+  create_table "menus", force: :cascade do |t|
+    t.datetime "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "options", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "organisations", force: :cascade do |t|
     t.string "name"
