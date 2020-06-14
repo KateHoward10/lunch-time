4a804788b8700fc3d154221e6523dac6d3de568d
Kate Howard <k.l.howard.1992@gmail.com>
2020-06-14 13:15:41 +0100

    Add form to create new menu

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,5 +1,36 @@
 class MenusController < ApplicationController
+  before_action :set_menu, only: [:show]
+  
   def index
     @menus = Menu.all
   end
+
+  def show
+  end
+
+  def new
+    @menu = Menu.new
+  end
+
+  def create
+    @menu = Menu.new(menu_params)
+    respond_to do |format|
+      if @menu.save
+        format.html { redirect_to @menu, notice: 'Menu was successfully created' }
+        format.json { render :show, status: :created, location: @menu }
+      else
+        format.html { render :new }
+        format.json { render json: @menu.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    def set_menu
+      @menu = Menu.find(params[:id])
+    end
+
+    def menu_params
+      params.require(:menu).permit(:date)
+    end
 end
