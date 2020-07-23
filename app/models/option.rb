a809fe0507689b6bc5f3173d3802879182e62f19
Kate Howard <k.l.howard.1992@gmail.com>
2020-07-23 20:46:12 +0100

    Associate option with order

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,4 +1,5 @@
 class Option < ApplicationRecord
   has_many :menu_options, dependent: :destroy
   has_many :menus, through: :menu_options
+  has_many :orders
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_19_182849) do
+ActiveRecord::Schema.define(version: 2020_07_23_170128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_07_19_182849) do
     t.text "notes"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "option_id"
   end
 
   create_table "organisations", force: :cascade do |t|
